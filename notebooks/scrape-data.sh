29533f169b4babb82d8b2a29bcc03aeb635844a0
Srihari Mohan <srihari_mohan@comcast.net>
2019-05-10 06:17:42 -0700

    adding notebooks for completeness

---
+++ notebooks/scrape-data.sh
@@ -0,0 +1,45 @@
+python3 perspective-api.py "abortion"
+sleep 120s
+python3 perspective-api.py "free speech college campuses"
+sleep 120s
+python3 perspective-api.py "medicare for all"
+sleep 120s
+python3 perspective-api.py "david hogg"
+sleep 120s
+python3 perspective-api.py "intersectionality"
+sleep 120s
+python3 perspective-api.py "rashida tlaib"
+sleep 120s
+python3 perspective-api.py "fox news"
+sleep 120s
+python3 perspective-api.py "cnn"
+sleep 120s
+python3 perspective-api.py "msnbc"
+sleep 120s
+python3 perspective-api.py "second ammendment"
+sleep 120s
+python3 perspective-api.py "white nationalist"
+sleep 120s
+python3 perspective-api.py "black lives matter"
+sleep 120s
+python3 perspective-api.py "jordan peterson"
+sleep 120s
+python3 perspective-api.py "tucker carlson"
+sleep 120s
+python3 perspective-api.py "rachel maddow"
+sleep 120s
+python3 perspective-api.py "MAGA"
+sleep 120s
+python3 perspective-api.py "censorship"
+sleep 120s
+python3 perspective-api.py "capitalism"
+sleep 120s
+python3 perspective-api.py "socialism"
+sleep 120s
+python3 perspective-api.py "democrats"
+sleep 120s
+python3 perspective-api.py "republicans"
+sleep 120s
+python3 perspective-api.py "#alexjones"
+sleep 120s
+python3 perspective-api.py "ben shapiro"
